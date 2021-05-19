fc1f183467f079d0a521189930c8bbf8821eeffd
maxym-valor <maxym.riabchuk@valor-software.com>
2021-05-19 15:41:18 +0300

    test: fixed e2e for split-button core

--- e2e/wdio/core/pages/split-button.po.ts
+++ e2e/wdio/core/pages/split-button.po.ts
@@ -13,7 +13,7 @@ export class SplitButtonPo extends CoreBaseComponentPo {
     buttonTemplateExample = 'fd-split-button-template-example ';
 
     mainbtn = '.fd-button__text'
-    arrowDownBtn = 'fd-split-button .fd-button:nth-of-type(2)';
+    arrowDownBtn = 'fd-split-button .fd-button:nth-of-type(2)' ;
     button = '.fd-button';
     splitMenu = 'div.fd-popover__popper'
     splitItem = '.fd-menu__list li';

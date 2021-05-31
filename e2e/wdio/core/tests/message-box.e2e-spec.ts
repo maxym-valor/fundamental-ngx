b34a46d98f78d1ee57f99194345abafdabd66c24
maxym-valor <maxym.riabchuk@valor-software.com>
2021-05-31 14:33:35 +0300

    Revert "Delete message-box.e2e-spec.ts"
    This reverts commit 06a228889c2a5d58317ac0d732fc30d919091a09.

--- e2e/wdio/core/tests/message-box.e2e-spec.ts
+++ e2e/wdio/core/tests/message-box.e2e-spec.ts
@@ -8,7 +8,7 @@ import {
     isElementDisplayed
 } from '../../driver/wdio';
 import {
-    iconsArr, buttonClassArr, sectionsArr
+    iconsArr, buttonClassArr
 } from '../fixtures/appData/message-box'
 
 describe('Message-box test suits', function () {
@@ -24,18 +24,20 @@ describe('Message-box test suits', function () {
     }, 1);
 
     it('Should check working of message-boxes', () => {
-        for (let i = 0; i < sectionsArr.length; i++) {
-            checkMessageBoxWorking(sectionsArr[i])
-        }
+        checkMessageBoxWorking(basedObjectExample);
+        checkMessageBoxWorking(mobileExample);
+        checkMessageBoxWorking(positionExample);
+        checkMessageBoxWorking(basedComponentExample);
+        checkMessageBoxWorking(openTemplateExample);
+        checkMessageBoxWorking(sematicTypesExample);
+        checkMessageBoxWorking(messageBoxExample);
+        checkMessageBoxWorking(complexTemplateExample);
     })
 
     it('Should check status after closing message-box', () => {
-        checkAcceptingMessage(basedObjectExample);
-        checkDismissingMessage(basedObjectExample);
-        checkAcceptingMessage(openTemplateExample);
-        checkDismissingMessage(openTemplateExample);
-        checkAcceptingMessage(basedComponentExample);
-        checkDismissingMessage(basedComponentExample);
+        checkResult(basedObjectExample);
+        checkResult(openTemplateExample);
+        checkResult(basedComponentExample);
     })
 
     it('Should check message & button types', () => {
@@ -43,7 +45,7 @@ describe('Message-box test suits', function () {
         for (let i = 0; i < buttonsLength; i++) {
             expect(getElementClass(sematicTypesExample + button, i)).toContain(buttonClassArr[i], `Element type is not ${buttonClassArr[i]}`)
             click(sematicTypesExample + button, i)
-            i === buttonsLength - 1 ? expect(doesItExist(messageIcon)).toBe(false, 'Icon exists') : expect(getElementClass(messageIcon)).toContain(iconsArr[i], `Icon is not ${iconsArr[i]}`)
+            i !== buttonsLength - 1 ? expect(getElementClass(messageIcon)).toContain(iconsArr[i], `Icon is not ${iconsArr[i]}`) : expect(doesItExist(messageIcon)).toBe(false, 'Icon exists')
             click(okButton)
         }
     })
@@ -52,7 +54,7 @@ describe('Message-box test suits', function () {
         messageBoxPage.checkRtlSwitch();
     });
 
-    describe('visual regression', function () {
+    describe('visual regression', function() {
         it('should check examples visual regression', () => {
             messageBoxPage.saveExampleBaselineScreenshot();
             expect(messageBoxPage.compareWithBaseline()).toBeLessThan(5);
@@ -69,14 +71,14 @@ describe('Message-box test suits', function () {
         }
     }
 
-    function checkAcceptingMessage(section: string): void {
+    function checkResult(section: string): void {
         click(section + button);
         click(okButton);
         section === basedObjectExample ? expect(getText(section + resultTxt)).toContain('Approved', 'Result is not OK') : expect(getText(section + resultTxt)).toContain('Ok', 'Result is not OK')
-    }
-    function checkDismissingMessage(section: string): void {
+        
         click(section + button);
         click(cancelButton);
         section === basedObjectExample ? expect(getText(section + resultTxt)).toContain('Canceled', 'Result is not Canceled') : expect(getText(section + resultTxt)).toContain('Cancel', 'Result is not Cancel')
     }
+
 });
